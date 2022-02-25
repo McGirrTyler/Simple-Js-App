249b52cc477a2e8eaf940d5ce68f7421abb27ee1
McGirrTyler <99841780+McGirrTyler@users.noreply.github.com>
2022-02-25 14:08:18 -0800

    JavaScript Array
    In this commit, I added // notes along code lines.
    Add conditional if,else if, else to display Pokémon's of all sizes.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let pokemonList = [{
+let pokemonList = [{ //Array for PokemonList
   name: "Bulbasaur",
   height: 0.7,
   type: ['Grass' , 'Poison']
@@ -14,5 +14,25 @@ let pokemonList = [{
   name: "Squirtle",
   height: 0.5,
   type: ['Water']
+},
+
+{
+  name: "Caterpie",
+  height: 0.3,
+  type: ['Bug']
 }];
-document.write(pokemonList);
+
+for (let i = 0; i < pokemonList.length; i++) { //Command will write list no matter how many items are added.
+  if (pokemonList[i].height < 0.4) { // Code to be used with smaller pokemons
+  console.log(pokemonList[i].name, + pokemonList[i].height + ' Look! A tiny pokemon. ' + pokemonList[i].type);
+  document.write(pokemonList[i].name);
+
+} else if (pokemonList[i].height > 1) { //Code will be used with larger pokemons
+  console.log(pokemonList[i].name, + pokemonList[i].height + ' WOAH! A huge pokemon! ' + pokemonList[i].type);
+  document.write(pokemonList[i].name);
+  
+} else { //Code that will be used for a majority of pokemons
+  console.log(pokemonList[i].name, + pokemonList[i].height, + pokemonList[i].type);
+  document.write(pokemonList[i].name);
+}
+}
