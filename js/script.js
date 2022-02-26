66cbbe3a7e71788794e169d468c7109bfb8649f6
McGirrTyler <99841780+McGirrTyler@users.noreply.github.com>
2022-02-25 19:40:41 -0800

    Update script.js
    In this commit all issues with array display was fixed. All elements of array show.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let pokemonList = [{ //Array for PokemonList
+let pokemonList = [{ //Array for Pokemon List
   name: "Bulbasaur",
   height: 0.7,
   type: ['Grass' , 'Poison']
@@ -24,15 +24,15 @@ let pokemonList = [{ //Array for PokemonList
 
 for (let i = 0; i < pokemonList.length; i++) { //Command will write list no matter how many items are added.
   if (pokemonList[i].height < 0.4) { // Code to be used with smaller pokemons
-  console.log(pokemonList[i].name, + pokemonList[i].height + ' Look! A tiny pokemon. ' + pokemonList[i].type);
-  document.write(pokemonList[i].name);
+  console.log(pokemonList[i].name, pokemonList[i].height + ' Look! A tiny pokemon. ', pokemonList[i].type);
+  document.write(pokemonList[i].name + "<br>", pokemonList[i].height + "<br>", pokemonList[i].type + "<br>",);
 
 } else if (pokemonList[i].height > 1) { //Code will be used with larger pokemons
-  console.log(pokemonList[i].name, + pokemonList[i].height + ' WOAH! A huge pokemon! ' + pokemonList[i].type);
-  document.write(pokemonList[i].name);
-  
+  console.log(pokemonList[i].name, pokemonList[i].height + ' WOAH! A huge pokemon! ',  pokemonList[i].type);
+  document.write(pokemonList[i].name + "<br>",);
+
 } else { //Code that will be used for a majority of pokemons
-  console.log(pokemonList[i].name, + pokemonList[i].height, + pokemonList[i].type);
-  document.write(pokemonList[i].name);
+  console.log(pokemonList[i].name, pokemonList[i].height,  pokemonList[i].type);
+  document.write(pokemonList[i].name + "<br>", pokemonList[i].height + "<br>", pokemonList[i].type + "<br>",);
 }
 }
