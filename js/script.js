1c24585f2038534026b9ef8990c0de444799c878
McGirrTyler <99841780+McGirrTyler@users.noreply.github.com>
2022-02-27 00:48:58 -0800

    Update script.js
    In this commit, I made two changes,
    First change, was to add a space within the Array - Type for better looking sentence.
    Second Change,  I added <p> withing my document.wrtie() again for better looking sentence

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let pokemonList = [{ //Array for Pokemon List
   name: "Bulbasaur",
   height: 0.7,
-  type: ['Grass' , 'Poison']
+  type: ['Grass' , ' Poison']
 },
 
 {
@@ -25,15 +25,15 @@ let pokemonList = [{ //Array for Pokemon List
 for (let i = 0; i < pokemonList.length; i++) { //Command will write list no matter how many items are added.
   if (pokemonList[i].height < 0.4) { // Code to be used with smaller pokemons
     console.log(pokemonList[i].name, pokemonList[i].height + ' Look! A tiny pokemon. ', pokemonList[i].type);
-    document.write(pokemonList[i].name + "<br>", pokemonList[i].height + ' Look! A tiny pokemon.' +
-    "<br>", pokemonList[i].type + "<br>",);
+    document.write("<p>" + pokemonList[i].name + "<br>", pokemonList[i].height + ' Look! A tiny pokemon.' +
+    "<br>", pokemonList[i].type + "<br>", "</p>");
 
   } else if (pokemonList[i].height > 1) { //Code will be used with larger pokemons
       console.log(pokemonList[i].name, pokemonList[i].height + ' WOAH! A huge pokemon! ',  pokemonList[i].type);
-      document.write(pokemonList[i].name + "<br>",);
+      document.write(pokemonList[i].name + "<br>", "</p>");
 
     } else { //Code that will be used for a majority of pokemons
       console.log(pokemonList[i].name, pokemonList[i].height,  pokemonList[i].type);
-      document.write(pokemonList[i].name + "<br>", pokemonList[i].height + "<br>", pokemonList[i].type + "<br>",);
+      document.write(pokemonList[i].name + "<br>", pokemonList[i].height + "<br>", pokemonList[i].type + "<br>", "</p>");
     }
 }
