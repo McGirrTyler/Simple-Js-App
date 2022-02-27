0639f16d2fb3b3c3bda9653ab6b79a0e1d48c56f
McGirrTyler <99841780+McGirrTyler@users.noreply.github.com>
2022-02-27 02:11:09 -0800

    Update script.js
    In this commit, I changed my for loop into an forEach loop.
    
    I also wrapped my array into an IIFE.
    
    More changes to come

--- js/script.js
+++ js/script.js
@@ -1,39 +1,43 @@
-let pokemonList = [{ //Array for Pokemon List
-  name: "Bulbasaur",
-  height: 0.7,
-  type: ['Grass' , ' Poison']
-},
+let pokemonRepository = (function () {
+  let pokemonList = [{ //Array for Pokemon List
+    name: "Bulbasaur",
+    height: 0.7,
+    type: ['Grass' , ' Poison']
+  },
 
-{
-  name: "Charmander",
-  height: 0.6,
-  type: ['Fire']
-},
+  {
+    name: "Charmander",
+    height: 0.6,
+    type: ['Fire']
+  },
 
-{
-  name: "Squirtle",
-  height: 0.5,
-  type: ['Water']
-},
+  {
+    name: "Squirtle",
+    height: 0.5,
+    type: ['Water']
+  },
 
-{
-  name: "Caterpie",
-  height: 0.3,
-  type: ['Bug']
+  {
+    name: "Caterpie",
+    height: 0.3,
+    type: ['Bug']
 }];
 
-for (let i = 0; i < pokemonList.length; i++) { //Command will write list no matter how many items are added.
-  if (pokemonList[i].height < 0.4) { // Code to be used with smaller pokemons
-    console.log(pokemonList[i].name, pokemonList[i].height + ' Look! A tiny pokemon. ', pokemonList[i].type);
-    document.write("<p>" + pokemonList[i].name + "<br>", pokemonList[i].height + ' Look! A tiny pokemon.' +
-    "<br>", pokemonList[i].type + "<br>", "</p>");
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-  } else if (pokemonList[i].height > 1) { //Code will be used with larger pokemons
-      console.log(pokemonList[i].name, pokemonList[i].height + ' WOAH! A huge pokemon! ',  pokemonList[i].type);
-      document.write(pokemonList[i].name + "<br>", "</p>");
+  function getAll() {
+    return pokemonList;
+  }
 
-    } else { //Code that will be used for a majority of pokemons
-      console.log(pokemonList[i].name, pokemonList[i].height,  pokemonList[i].type);
-      document.write(pokemonList[i].name + "<br>", pokemonList[i].height + "<br>", pokemonList[i].type + "<br>", "</p>");
-    }
-}
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+  function pokemonListLoop(pokemon) {
+    console.log(getAll());
+  }
+  pokemonList.forEach(pokemonListLoop);
