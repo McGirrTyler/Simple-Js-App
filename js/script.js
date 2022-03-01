2490c5b9c86e360463af79969093c7ac7c030cfa
McGirrTyler <99841780+McGirrTyler@users.noreply.github.com>
2022-03-01 12:35:11 -0800

    Update script.js
    In this commit, I changed name of variables to more logical consistent naming.
    
    Added an eventListener which is still being worked on.
    
    showDetails is also still in the works

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let pokemonRepository = (function () { //Start of IIFE
-  let pokemonList = [{ //Array for Pokemon List
+  let repository = [{ //Array for Pokemon List
     name: "Bulbasaur",
     height: 0.7,
     type: ['Grass' , ' Poison']
@@ -23,56 +23,73 @@ let pokemonRepository = (function () { //Start of IIFE
     type: ['Bug']
 }];
 
-  function add(pokemon) {
-    pokemonList.push(pokemon);
+  function add(pokemon) { // Function that allows for adding of Pokemons
+    repository.push(pokemon);
   }
 
-  function getAll() {
-    return pokemonList;
+  function getAll() { // Function that calls repository
+    return repository;
   }
 
-  return {
+  function addListItem(pokemon) { // Function used to call and dispaly pokemons
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listPokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+
+  }
+
+  function showDetails(pokemon) {
+    console.log(
+      pokemon.name,
+      pokemon.height,
+      pokemon.type
+    );
+  }
+
+  return { // Returns functions
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
-  pokemonRepository.getAll().forEach(function(pokemon) { //Function call for pokemonList
-    if (pokemon.height < 0.4) { // Command for smaller pokemons
-      console.log(
-        pokemon.name,
-        pokemon.height + " Look! A tiny pokemon! ",
-        pokemon.type
-      );
-      document.write(
-        "<p>" + pokemon.name + "<br>",
-        pokemon.height + " Look! A tiny pokemon! " + "<br>",
-        pokemon.type + "<br>",
-        "</p>"
-      );
-
-    } else if (pokemon.height > 1) { // Command for lager pokemons
+    // Start of main forEach
+    pokemonRepository.getAll().forEach(function(pokemon) { //Function call for pokemonList
+      if (pokemon.height < 0.4) { // Command for smaller pokemons
         console.log(
           pokemon.name,
-          pokemon.height + " Woah! A huge pokemon! ",
+          pokemon.height + " Look! A tiny pokemon! ",
           pokemon.type
         );
-        document.write(
-          "<p>" +
-          pokemon.name + "<br>",
-          pokemon.height + " Woah! A huge pokemon! " + "<br>",
-          pokemon.type + "<br>",
-          "</p>"
-        );
 
-      } else { // Command for majority of pokemons
-        console.log(pokemon.name, pokemon.height, pokemon.type);
-        document.write(
-          "<p>" +
-          pokemon.name + "<br>",
-          pokemon.height + "<br>",
-          pokemon.type + "<br>",
-          "</p>"
-        );
-      }
-  });
+      } else if (pokemon.height > 1) { // Command for lager pokemons
+          console.log(
+            pokemon.name,
+            pokemon.height + " Woah! A huge pokemon! ",
+            pokemon.type
+          );
+
+        } else { // Command for majority of pokemons
+          console.log(pokemon.name, pokemon.height, pokemon.type);
+        }
+    });
+
+    pokemonRepository.getAll().forEach(function(pokemon) {
+      pokemonRepository.addListItem(pokemon);
+    });
+
+    let button = document.querySelector("button");
+      button.addEventListener("click", function (event) {
+        console.log(event);
+      });
+
+    /* Potiental Code for Search Bar
+    document.querySelector('.show-more').addEventListener('click', function () {
+      document.querySelector('.additional-information')
+      .classList.toggle('is-visible');
+    });
+    */
