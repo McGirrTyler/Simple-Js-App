5f61c63b71bb0ed50e10e17219297c73eb60ad48
McGirrTyler <99841780+McGirrTyler@users.noreply.github.com>
2022-07-05 14:52:54 -0700

    Update script.js
    In this commit, fucntion for open and close modal was added. Dialog functions  has been added to some extent.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let pokemonRepository = (function () { //Start of IIFE
   let pokemonList = []; // Pokemon Array to be fetched
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/limit=150';
   function add(pokemon) { // Function that allows for adding of Pokemons
     if (
       typeof pokemon === "object" &&
@@ -47,7 +46,7 @@ let pokemonRepository = (function () { //Start of IIFE
           };
           add(pokemon);
         });
-      }).catch(function (e) { 
+      }).catch(function (e) {
         console.error(e);
       })
     }
@@ -82,12 +81,76 @@ let pokemonRepository = (function () { //Start of IIFE
       });
     });
 
+  function showModal(title, text) {
+    let modalContainer = document.querySelector('#modal-container');
+      // Clear all existing modal content
+    modalContainer.innerHTML = '';
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
 
+      // Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
 
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
 
-    /* Potiental Code for Search Bar
-    document.querySelector('.show-more').addEventListener('click', function () {
-      document.querySelector('.additional-information')
-      .classList.toggle('is-visible');
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+    modalContainer.classList.add('is-visible');
+}
+
+  function hideModal() {
+  let modalContainer = document.querySelector('#modal-container');
+  modalContainer.classList.remove('is-visible');
+}
+
+  function showDialog(title, text) {
+    showModal(title, text);
+      //defined modalContainer here
+    let modalContainer = document.querySelector('#modal-container');
+      //to add a confirm and cancel button to the modal
+    let modal = modalContainer.querySelector('.modal');
+
+    let confirmButton = document.createElement('button');
+    confirmButton.classList.add('modal-confirm');
+    confirmButton.innerText = 'Confirm';
+
+    let cancelButton = document.createElement('button');
+    cancelButton.classList.add('modal-cancel');
+    cancelButton.innerText = 'Cancel';
+
+    modal.appendChild(confirmButton);
+    modal.appendChild(cancelButton);
+
+    // We want to focus the confirmButton so that the user can simply press Enter
+    confirmButton.focus();
+  }
+
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+      }
     });
-    */
+
+  let modalContainer = document.querySelector('#modal-container');
+  modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay
+  let target = e.target;
+  if (target === modalContainer) {
+    hideModal();
+  }
+});
+
+document.querySelector('#show-modal').addEventListener('click', () => {
+  showModal('Modal title', 'This is the modal content!');
+});
